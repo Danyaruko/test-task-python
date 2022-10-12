6d778db377c5a0cdac33a04d76e164beb44ee398
Danyaruko <danilpatana1905@gmail.com>
2022-10-12 15:42:17 +0300

    Define create and update methods for serializers

--- Backend/test_backend/users/serializers.py
+++ Backend/test_backend/users/serializers.py
@@ -9,8 +9,26 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.group = validated_data.get('group', instance.group)
+        instance.save()
+        return instance
+
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = '__all__'
+
+    def create(self, validated_data):
+        return Group.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.save()
+        return instance
